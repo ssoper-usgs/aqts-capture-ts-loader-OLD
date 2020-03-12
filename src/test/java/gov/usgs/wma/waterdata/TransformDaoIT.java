3158e29e630b1cda8c2b0de5c171f221d28c491b
ssoper <ssoper@usgs.gov>
2020-03-12 13:12:08 -0500

    test failing, seems to be a mapping issue

--- src/test/java/gov/usgs/wma/waterdata/TransformDaoIT.java
+++ src/test/java/gov/usgs/wma/waterdata/TransformDaoIT.java
@@ -1,10 +1,17 @@
 package gov.usgs.wma.waterdata;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -46,18 +53,78 @@ import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 public class TransformDaoIT {
 
 	@Autowired
-	private TransformDao transformDao;
-	private RequestObject request = new RequestObject();
+	public TransformDao transformDao;
 
-	private static final String tsUniqueId = "17f83e62b06e4dc29e78d96b4426a255";
+	public RequestObject request;
+	List<Map<String, Object>> timeSeriesList;
+	public static final String tsUniqueId = "17f83e62b06e4dc29e78d96b4426a255";
+
+	@BeforeEach
+	public void setup() {
+		request = new RequestObject();
+		request.setUniqueId(tsUniqueId);
+
+		Map<String, Object> timeSeries1;
+		{
+			timeSeries1 = new HashMap<>();
+			timeSeries1.put("groundwater_daily_value_identifier", "USGS-132624144452771-17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries1.put("time_series_unique_id", "17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries1.put("monitoring_location_identifier", "USGS-132624144452771");
+			timeSeries1.put("observered_property_id", null);
+			timeSeries1.put("statistic_id", null);
+			timeSeries1.put("time_step", 2008-06-03);
+			timeSeries1.put("unit_of_measure", "ft");
+			timeSeries1.put("result", 36.02);
+			timeSeries1.put("approvals", "Approved");
+			timeSeries1.put("qualifiers", "null");
+			timeSeries1.put("grades", "50");
+
+		}
+		Map<String, Object> timeSeries2;
+		{
+			timeSeries2 = new HashMap<>();
+			timeSeries2.put("groundwater_daily_value_identifier", "USGS-132624144452771-17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries2.put("time_series_unique_id", "17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries2.put("monitoring_location_identifier", "USGS-132624144452771");
+			timeSeries2.put("observered_property_id", null);
+			timeSeries2.put("statistic_id", null);
+			timeSeries2.put("time_step", 2008-06-04);
+			timeSeries2.put("unit_of_measure", "ft");
+			timeSeries2.put("result", 35.96);
+			timeSeries2.put("approvals", "Approved");
+			timeSeries2.put("qualifiers", null);
+			timeSeries2.put("grades", "50");
+		}
+		Map<String, Object> timeSeries3;
+		{
+			timeSeries3 = new HashMap<>();
+			timeSeries3.put("groundwater_daily_value_identifier", "USGS-132624144452771-17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries3.put("time_series_unique_id", "17f83e62b06e4dc29e78d96b4426a255");
+			timeSeries3.put("monitoring_location_identifier", "USGS-132624144452771");
+			timeSeries3.put("observered_property_id", null);
+			timeSeries3.put("statistic_id", null);
+			timeSeries3.put("time_step", 2008-06-04);
+			timeSeries3.put("unit_of_measure", "ft");
+			timeSeries3.put("result", 35.96);
+			timeSeries3.put("approvals", "Approved");
+			timeSeries3.put("qualifiers", null);
+			timeSeries3.put("grades", "50");
+		}
+
+		timeSeriesList = new ArrayList<>();
+		timeSeriesList.add(timeSeries1);
+		timeSeriesList.add(timeSeries2);
+		timeSeriesList.add(timeSeries3);
+	}
 
 	@Test
 	public void testGet() {
 
 		// get new data, return unique ids
-		request.setUniqueId(tsUniqueId);
+		System.out.println(request.getUniqueId());
 		List<Map<String, Object>> actualData = transformDao.getTimeSeries(request.getUniqueId());
 		assertNotNull(actualData);
 		System.out.println(actualData);
+		assertThat(actualData, containsInAnyOrder(timeSeriesList));
 	}
 }
